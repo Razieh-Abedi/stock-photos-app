a83a051b2caeb3e62e38c759a5c83b4d112ed393
Razieh Abedi <raziyeabedi@gmail.com>
2022-11-14 15:50:53 +0800

    debugged the search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Photo from "./Photo";
 import { FaSearch } from "react-icons/fa";
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
-const mainUrl = "https://api.unsplash.com/photos/";
-const searchUrl = "https://api.unsplash.com/search/photos/";
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
   const [query, setQuery] = useState("");
+  const mounted = useRef(false);
+  const [newImages, setNewImages] = useState(false);
   // useEffect(() => {
   //   setLoading(true);
   //   let url;
@@ -36,24 +38,10 @@ function App() {
   //     });
   // }, [page]);
 
-  useEffect(() => {
-    const event = window.addEventListener("scroll", () => {
-      if (
-        !loading &&
-        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
-      ) {
-        setPage((oldPage) => {
-          return oldPage + 1;
-        });
-      }
-    });
-    return () => window.removeEventListener("scroll", event);
-  }, []);
-
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    const urlPage = `?page=1${page}`;
+    const urlPage = `&page=1${page}`;
     const urlQuery = `&query=${query}`;
     if (query) {
       url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
@@ -72,19 +60,51 @@ function App() {
           return [...oldPhotos, ...data];
         }
       });
+      setNewImages(false);
       setLoading(false);
     } catch (error) {
+      setNewImages(false);
       setLoading(false);
-      console.log(error);
     }
   };
   useEffect(() => {
     fetchImages();
   }, [page]);
+
+  useEffect(() => {
+    if (!mounted.current) {
+      mounted.current = true;
+      return;
+    }
+    if (!newImages) return;
+    if (loading) return;
+    setPage((oldPage) => {
+      return oldPage + 1;
+    });
+  }, [newImages]);
+
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      setNewImages(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", event);
+    return () => window.removeEventListener("scroll", event);
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setPage(1);
+    if (!query) {
+      return;
+    }
+    if (page === 1) {
+      fetchImages();
+      setPage(1);
+    }
   };
+
   return (
     <main>
       <section className="search">
@@ -103,8 +123,8 @@ function App() {
       </section>
       <section className="photos">
         <div className="photos-center">
-          {photos.map((item) => {
-            return <Photo key={item.id} {...item} />;
+          {photos.map((item, index) => {
+            return <Photo key={index} {...item} />;
           })}
         </div>
       </section>
